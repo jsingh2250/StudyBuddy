f91f8761e4c67d5e8a403db6196216262afec68f
Brendan Spranger <bspranger76@gmail.com>
2022-10-23 16:01:21 -0400

    Test commit of cloned repo

--- src/StudyBuddy.java
+++ src/StudyBuddy.java
@@ -1,5 +1,5 @@
 public class StudyBuddy {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, StudyBuddy!");
     }
 }
